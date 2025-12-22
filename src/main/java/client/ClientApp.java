303a43b814c5a7db9efccb1af7d55e20b08f9f23
MatiasVinkovic <mativink@gmail.com>
2025-12-23 00:08:29 +0100

    exemple code doc

--- src/main/java/client/ClientApp.java
+++ src/main/java/client/ClientApp.java
@@ -2,7 +2,6 @@ package client;
 
 import com.saf.core.*;
 import com.saf.messages.OrderRequest;
-import com.saf.messages.OrderResponse;
 import com.saf.spring.RestRemoteActorRef;
 import com.saf.spring.SAF;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,43 +13,33 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ClientApp {
 
     public static void main(String[] args) throws Exception {
-        // 1. Démarrage du système (Console désactivée)
+        // 1. Code minimum
         ActorSystem system = SAF.start(ClientApp.class, "ms-client", false, args);
         DiscoveryClient dc = SAF.getContext().getBean(DiscoveryClient.class);
 
-        System.out.println("[SAF] Attente de synchronisation Eureka...");
-        Thread.sleep(5000);
 
-        // 1. Création de la référence vers l'acteur distant
-        ActorRef resto = new RestRemoteActorRef(dc, "ms-restaurant", "tom");
+        // Je créer un acteur local, un client par exemple ici
+        //si jamais je voulais que mon acteur soit un autre acteur,
+        // il suffirait de changer ClientActor.class par le nom de l'autre classe d'acteur que je créerai par exemple LivreurActor.class
+        system.createActor(ClientActor.class, "Matias");
 
-        // 2. Test du mode SYNCHRONE (ask) exigé par le PDF
-        System.out.println("[CLIENT] Envoi d'une commande synchrone...");
-        resto.ask(new OrderRequest("Pizza", "Matias"), OrderResponse.class)
-                .thenAccept(res -> {
-                    System.out.println("[CLIENT] Confirmation reçue : " + res.getStatus());
-                });
+        // c'est mieux d'attendre 2 secondes pour être sûr que le service Eureka est bien synchronisé
+        System.out.println("[INFO] Attente de la synchronisation Eureka (2s)...");
+        Thread.sleep(2000);
 
-//        // 2. Récupération du DiscoveryClient (nécessaire pour la résolution Eureka)
-//        DiscoveryClient dc = SAF.getContext().getBean(DiscoveryClient.class);
-//
-//        // 3. Création de l'acteur local
-//        system.createActor(ClientActor.class, "Matias");
-//
-//        // --- ATTENTION : On laisse le temps à Eureka de découvrir le restaurant ---
-//        System.out.println("[INFO] Attente de la synchronisation Eureka (10s)...");
-//        Thread.sleep(2000);
-//
-//        // 4. Création de la référence distante (Équivalent de la commande 'ref')
-//        // On cible le service MS-RESTAURANT et l'acteur ChefGusto
-//        ActorRef restoRef = new RestRemoteActorRef(dc, "MS-RESTAURANT", "tom");
-//
-//        // 5. Création et envoi du message (Équivalent de 'msg' + 'tell')
-//        System.out.println("[ACTION] Envoi de la commande de Pizza...");
-//        OrderRequest commande = new OrderRequest("Pizza Royale", "Matias");
-//
-//        restoRef.tell(commande);
-//
-//        System.out.println("[OK] Message envoyé au restaurant via le code.");
+        // 4. Création de la référence distante, je veux parler à l'acteur tom que j'ai créer chez le restaurant
+        // On cible le service MS-RESTAURANT et l'acteur tom
+        // dc, c'est eureka
+        ActorRef restoRef = new RestRemoteActorRef(dc, "MS-RESTAURANT", "tom");
+
+        // un acteur envoie un message, on peut imaginer qu'il envoie un message de type OrderRequest, mais
+        //encore une fois, ça peut être n'impporte quoi, par exemple CreationTicket, etc.
+        System.out.println("[ACTION] Envoi de la commande de Pizza...");
+        OrderRequest commande = new OrderRequest("Pizza Royale", "Matias");
+
+        // On envoie le message à l'acteur distant simplement
+        restoRef.tell(commande);
+
+        System.out.println("[OK] Message envoyé au restaurant via le code.");
     }
 }
